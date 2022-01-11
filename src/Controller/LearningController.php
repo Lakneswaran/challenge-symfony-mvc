ed85e761e5db5ec541bedb3ce4ca4cfa921c3489
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-01-11 09:36:08 +0100

    step 3 unfinished

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -4,24 +4,62 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LearningController extends AbstractController
 {
-    #[Route('/aboutme', name: 'aboutMe')]
+    #[Route('/learning', name: 'learning')]
     public function index(): Response
     {
-        return $this->render('aboutme/index.html.twig', [
+        return $this->render('learning/index.html.twig', [
             'controller_name' => 'LearningController',
         ]);
     }
 
 
-    #[Route('/', name: 'Homepage')]
-    public function aboutme(): Response
+    #[Route('/aboutMe', name: 'Homepage')]
+    public function aboutMe(): Response
     {
-        return $this->render('aboutme/index.html.twig', [
+        return $this->render('showMyName/index.html.twig', [
             'name' => 'becode',
         ]);
+    }
+
+    /**
+     * @Route("/", name="showMyName")
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function showMyName(SessionInterface $session): Response
+    {
+       if($session->get("name")){
+            $name = $session->get("name");
+           
+
+       }
+       else{
+           $name = "unknown";
+       }
+
+       return $this->render('showMyName.html.twig', [
+        'name' => $name,
+       ]);
+    }
+
+    /**
+     * @Route("/changeMyName", name="changeMyName", methods={"POST"})
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return RedirectResponse
+     */
+    public function changeMyName(Request $request,SessionInterface $session):RedirectResponse
+    {
+        $name= $request->request->get('name');
+        $session->set('name',$name);
+        return $this->redirectToRoute('showMyName');
     }
 }
